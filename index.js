c8298c217c234b7833f950ec157d3f59870b12ad
Igor <ingvardm@gmail.com>
2017-11-28 06:42:00 +0200

    aaa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
-const app = express
+const app = express()
 const port = process.env.PORT || 5000
 
 app.set('port', port)
