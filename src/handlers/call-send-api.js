8f61a8544f15cc6ecfa4403cb2527aef63bd36b8
Igor <ingvardm@gmail.com>
2017-11-28 20:29:02 +0200

    refactor

--- src/handlers/call-send-api.js
+++ src/handlers/call-send-api.js
@@ -1,22 +1,18 @@
 const { facebookPostMessage } = require('../../config')
 const request = require('request')
 
+const error = e => {
+    if (!e) console.log('message sent!')
+    else console.error(`Unable to send message: ${e}`)
+}
+
 const callSendAPI = (sender_psid, response) => {
     let message = facebookPostMessage
     message.json = {
-        "recipient": {
-            "id": sender_psid
-        },
+        "recipient": { "id": sender_psid },
         "message": response
     }
-
-    request(, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
+    request(message, error)
 }
 
 module.exports = callSendAPI
--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,5 @@
 const message = require('./message')
 const postBack = require('./post-back')
-const callSendAPI = require('./call-send-api')
 const subscribe = require('./subscribe')
 
 module.exports = { message, subscribe, postBack }
