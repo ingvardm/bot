1c1c404bbdab2a04422c32586c2d8e53aa126301
Igor <ingvardm@gmail.com>
2017-11-28 18:02:44 +0200

    bugs

--- src/routs/index.js
+++ src/routs/index.js
@@ -1,3 +1,5 @@
+const {FB_VERIFY_TOKEN, FB_PAGE_ACCESS_TOKEN} = require('../tokens')
+
 module.exports = [
     {
         path: '/',
@@ -6,32 +8,16 @@ module.exports = [
     {
         path: '/webhook/',
         callback: (req, res) => {
-            
-              // Your verify token. Should be a random string.
-              let VERIFY_TOKEN = "enkqwenfkev"
-                
-              // Parse the query params
-              let mode = req.query['hub.mode'];
-              let token = req.query['hub.verify_token'];
-              let challenge = req.query['hub.challenge'];
-
-              console.log(req.query)
-                
-              // Checks if a token and mode is in the query string of the request
-              if (mode && token) {
-              
-                // Checks the mode and token sent is correct
-                if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-                  
-                  // Responds with the challenge token from the request
-                  console.log('WEBHOOK_VERIFIED');
-                  res.status(200).send(challenge);
-                
-                } else {
-                  // Responds with '403 Forbidden' if verify tokens do not match
-                  res.sendStatus(403);
-                }
-              } res.sendStatus(403);
+            let mode = req.query['hub.mode']
+            let token = req.query['hub.verify_token']
+            let challenge = req.query['hub.challenge']
+            console.log(mode, mode === 'subscribe' && token === FB_VERIFY_TOKEN, FB_VERIFY_TOKEN)
+            if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
+                console.log('WEBHOOK_VERIFIED')
+                res.status(200).send(challenge)
             }
+            console.log('VERIFICATION_ERROR ' + req.query)
+            res.sendStatus(403)
+        }
     }
 ]
---
+++ src/tokens.js
@@ -0,0 +1,4 @@
+module.exports = {
+    FB_PAGE_ACCESS_TOKEN: process.env.FB_PAGE_ACCESS_TOKEN,
+    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN
+}
