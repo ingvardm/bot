66721015618f303fb827e3b28332b457dc7a1f08
Igor <ingvardm@gmail.com>
2017-11-28 20:30:49 +0200

    refactor

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ module.exports = {
         "uri": "https://graph.facebook.com/v2.6/me/messages",
         "qs": { "access_token": tokens.FB_PAGE_ACCESS_TOKEN },
         "method": "POST",
-        "json": request_body
+        "json": ""
     }
 }
