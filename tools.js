d283fcb025f689ac181dbec9f278c189f6d89641
Igor <ingvardm@gmail.com>
2017-11-29 02:43:51 +0200

    added bot

--- tools.js
+++ tools.js
@@ -3,8 +3,8 @@ const { tokens } = require('./config')
 console.log(tokens)
 
 const getMissingTokens = () => Object.keys(tokens).filter(token => {
-    console.log(!tokens.token)
-    return !tokens.token
+    console.log(tokens.token === undefined)
+    return tokens.token === undefined
 })
 const logErrorAndExit = message => {
     console.error(message)
