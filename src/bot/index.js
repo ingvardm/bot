46d9b4d0df7cca566c2f66637b1b4a580c8da334
Igor <ingvardm@gmail.com>
2017-11-29 02:48:03 +0200

    added bot

--- src/bot/index.js
+++ src/bot/index.js
@@ -20,6 +20,16 @@ const initialize = (port) => {
         let { trigger, actions, callback } = entry
         controller.hears(trigger, actions, callback);
     })
+
+    controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
+        controller.storage.users.get(message.user, function(err, user) {
+            if (user && user.name) {
+                bot.reply(message, 'Hello ' + user.name + '!!');
+            } else {
+                bot.reply(message, 'Hello.');
+            }
+        });
+    });
 }
 
 module.exports = { initialize }
--- tools.js
+++ tools.js
@@ -1,7 +1,5 @@
 const { tokens } = require('./config')
 
-console.log(tokens)
-
 const getMissingTokens = () => Object.keys(tokens).filter(token => !tokens[token])
 const logErrorAndExit = message => {
     console.error(message)
