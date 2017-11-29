ac0bd2d1319148e58352b9f18718307a98517708
Igor <ingvardm@gmail.com>
2017-11-29 07:45:30 +0200

    added mongodb

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
-const tokens = {
-    FB_PAGE_ACCESS_TOKEN: process.env.FB_PAGE_ACCESS_TOKEN,
-    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
-    APP_SECRET: process.env.APP_SECRET,
-}
-
-const db = {
-    uri: process.env.MONGODB_URI
-}
-
+const { FB_PAGE_ACCESS_TOKEN, FB_VERIFY_TOKEN, APP_SECRET, MONGODB_URI } = process.env
+const tokens = { FB_PAGE_ACCESS_TOKEN, FB_VERIFY_TOKEN, APP_SECRET }
+const db = { uri: MONGODB_URI }
 const port = process.env.PORT || 8080
 
 module.exports = { tokens, port, db }
